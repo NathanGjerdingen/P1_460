ae3ce969c18e450d9c927438809c07037a61f220
Nathan <nate.gjerd@icloud.com>
2019-02-16 10:21:04 -0600

    Found resource online
    Found resource online, imported it and got rid of bugs. Will see if it applies...

---
+++ src/stopwaitsender.java
@@ -0,0 +1,53 @@
+//
+//	CREDIT TO: https://gangof1994.wordpress.com/2015/08/11/stop-and-wait-protocol-using-socket-program/
+//	
+//	EDITED BY: Nathan Gjerdingen
+//
+
+//	SENDER
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+class stopwaitsender {
+
+	public static void main(String args[]) throws Exception {
+		stopwaitsender sws = new stopwaitsender();
+		sws.run();
+	}
+
+	public void run() throws Exception {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter no of frames to be sent:");
+		int n = sc.nextInt();
+		Socket myskt = new Socket("localhost",9999);
+		PrintStream myps = new PrintStream(myskt.getOutputStream());
+
+		for (int i = 0 ; i <= n;) {
+			if (i == n) { 
+				myps.println("exit");
+				break;
+			}
+			
+			System.out.println("Frame no " + i + "is sent");
+			myps.println(i);
+
+			BufferedReader bf = new BufferedReader(new InputStreamReader(myskt.getInputStream()));
+
+			String ack = bf.readLine();
+
+			if(ack != null) {
+				System.out.println("Acknowledgement was Received from receiver");
+				i++;
+				Thread.sleep(4000);
+			} else {
+				myps.println(i);
+			}
+		}
+		
+		// Closes to suppress warnings...
+		sc.close();
+		myskt.close();
+	}
+}
