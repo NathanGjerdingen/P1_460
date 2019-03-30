a1cb0312fe3d3eafcdf448b1e01c8b27cc9211ee
Nathan <nate.gjerd@icloud.com>
2019-03-30 17:30:18 -0500

    Implemented ACK
    implemented ACK in reciever

--- src/PacketReceiver.java
+++ src/PacketReceiver.java
@@ -7,47 +7,47 @@ import java.io.FileOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 
 
 class PacketReceiver {
-	
-//	HOW TO RUN PROGRAM EXAMPLE:
-//	java PacketReceiver -d 0.5 20.20.20.20 8024
-	
-	
-//	THINGS NEEDED TO BE DONE:
-//	1. Implement drop/currupt/ rate in recieving
-//	2. Implement acknowledgment to Sender
-	
-//	LET'S 'A GO~
-//	
-//	─────────────────────███████──███████
-//	─────────────────████▓▓▓▓▓▓████░░░░░██
-//	───────────────██▓▓▓▓▓▓▓▓▓▓▓▓██░░░░░░██
-//	─────────────██▓▓▓▓▓▓████████████░░░░██
-//	───────────██▓▓▓▓▓▓████████████████░██
-//	───────────██▓▓████░░░░░░░░░░░░██████
-//	─────────████████░░░░░░██░░██░░██▓▓▓▓██
-//	─────────██░░████░░░░░░██░░██░░██▓▓▓▓██
-//	───────██░░░░██████░░░░░░░░░░░░░░██▓▓██
-//	───────██░░░░░░██░░░░██░░░░░░░░░░██▓▓██
-//	─────────██░░░░░░░░░███████░░░░██████
-//	───────────████░░░░░░░███████████▓▓██
-//	─────────────██████░░░░░░░░░░██▓▓▓▓██
-//	───────────██▓▓▓▓██████████████▓▓██
-//	─────────██▓▓▓▓▓▓▓▓████░░░░░░████
-//	───────████▓▓▓▓▓▓▓▓██░░░░░░░░░░██
-//	───────████▓▓▓▓▓▓▓▓██░░░░░░░░░░██
-//	───────██████▓▓▓▓▓▓▓▓██░░░░░░████████
-//	─────────██████▓▓▓▓▓▓████████████████
-//	───────────██████████████████████▓▓▓▓██
-//	─────────██▓▓▓▓████████████████▓▓▓▓▓▓██
-//	───────████▓▓██████████████████▓▓▓▓▓▓██
-//	───────██▓▓▓▓██████████████████▓▓▓▓▓▓██
-//	───────██▓▓▓▓██████████──────██▓▓▓▓████
-//	───────██▓▓▓▓████──────────────██████ 
-//	─────────████
+
+	//	HOW TO RUN PROGRAM EXAMPLE:
+	//	java PacketReceiver -d 0.5 20.20.20.20 8024
+
+	//	THINGS NEEDED TO BE DONE:
+	//	1. Implement drop/corrupt/discard rate in dataReceiver
+	//	2. Get rid of leftover packet bullshit
+
+	//					  LET'S 'A GO~
+	//	
+	//	─────────────────────███████──███████
+	//	─────────────────████▓▓▓▓▓▓████░░░░░██
+	//	───────────────██▓▓▓▓▓▓▓▓▓▓▓▓██░░░░░░██
+	//	─────────────██▓▓▓▓▓▓████████████░░░░██
+	//	───────────██▓▓▓▓▓▓████████████████░██
+	//	───────────██▓▓████░░░░░░░░░░░░██████
+	//	─────────████████░░░░░░██░░██░░██▓▓▓▓██
+	//	─────────██░░████░░░░░░██░░██░░██▓▓▓▓██
+	//	───────██░░░░██████░░░░░░░░░░░░░░██▓▓██
+	//	───────██░░░░░░██░░░░██░░░░░░░░░░██▓▓██
+	//	─────────██░░░░░░░░░███████░░░░██████
+	//	───────────████░░░░░░░███████████▓▓██
+	//	─────────────██████░░░░░░░░░░██▓▓▓▓██
+	//	───────────██▓▓▓▓██████████████▓▓██
+	//	─────────██▓▓▓▓▓▓▓▓████░░░░░░████
+	//	───────████▓▓▓▓▓▓▓▓██░░░░░░░░░░██
+	//	───────████▓▓▓▓▓▓▓▓██░░░░░░░░░░██
+	//	───────██████▓▓▓▓▓▓▓▓██░░░░░░████████
+	//	─────────██████▓▓▓▓▓▓████████████████
+	//	───────────██████████████████████▓▓▓▓██
+	//	─────────██▓▓▓▓████████████████▓▓▓▓▓▓██
+	//	───────████▓▓██████████████████▓▓▓▓▓▓██
+	//	───────██▓▓▓▓██████████████████▓▓▓▓▓▓██
+	//	───────██▓▓▓▓██████████──────██▓▓▓▓████
+	//	───────██▓▓▓▓████──────────────██████ 
+	//	─────────████
 
 	//	Initialize static variables with default vals...
 	static int datagramsToCurrupt = 0;
@@ -55,17 +55,17 @@ class PacketReceiver {
 	static int receiver_port = 8024;
 
 	private static void setArgs(String[] args) throws UnknownHostException {
-		
+
 		//	Test if we want to set...
 		if (args.length > 3 && args[0].endsWith("d")) {
-			
+
 			//	Set static variable from args...
 			datagramsToCurrupt = (int) (Double.parseDouble(args[1]) * 100);
 			receiver_ip_addr = InetAddress.getByName(args[2]);
 			receiver_port = Integer.parseInt(args[3]);
-			
+
 		}
-		
+
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -73,20 +73,19 @@ class PacketReceiver {
 		//	Set args...
 		setArgs(args);
 
-		// Initialize Buffer sizes
-		byte[] data = new byte[10000];
+		// Initialize data sizes and Datagram Packets for storage
+		byte[] currentData = new byte[10000];
+		DatagramPacket dataRecieved = new DatagramPacket(currentData, currentData.length);
+
+		// EVENTUALLY NEEDS TO BE REMOVED IF POSSIBLE.
 		byte[] leftoverData = new byte[7284];
-
-		// Final buffer size to contain data (data + leftoverData) 
-		byte[] finalData = new byte[107273];	
-
-		//	Datagram Packets for storage
-		DatagramPacket dataPacket = new DatagramPacket(data, data.length);
 		DatagramPacket leftoverDataPacket = new DatagramPacket(leftoverData, leftoverData.length);
 
+		// Final data size to contain data (data + leftoverData) 
+		byte[] finalData = new byte[107273];	
+
 		//	Eventually want this syntax below...
-		//	DatagramSocket socket = new DatagramSocket(receiver_port, receiver_ip_addr);
-		DatagramSocket dataReciever = new DatagramSocket(receiver_port);
+		DatagramSocket dataReciever = new DatagramSocket(receiver_port, receiver_ip_addr);
 
 		//	Initial looping value and size...
 		int loopCounter = 0;
@@ -94,18 +93,18 @@ class PacketReceiver {
 
 		//	Starting output...
 		System.out.println("packets received:");
-		
+
 		//-------------------------------------------------------
 		//														|
 		// 	AREA BELOW IS WHERE SHIT IS DONE					|
 		//														|
 		//-------------------------------------------------------
-		
+
 		while (loopCounter < 11) {
 
 			// final clause for leftover info...
 			if (loopCounter == 10) {
-				
+
 				//	Recieve info...
 				dataReciever.receive(leftoverDataPacket);
 				leftoverData = leftoverDataPacket.getData();
@@ -117,27 +116,33 @@ class PacketReceiver {
 				//	Ouptut info...
 				System.out.println("Sequence number: " + leftoverData[0] +", Offset start: " + startSize + ", Offset end: " + size);
 				System.arraycopy(leftoverData, 1, finalData, 0+(loopCounter*9999), 7283);
-				
+
+				//	Send Acknowledgement Packet back to Data Sender
+				dataReciever.send(new DatagramPacket(new byte[] {0}, 1, new InetSocketAddress("localhost", 8080)));
+
 			} else {
 
 				//	When recieving data...
-				dataReciever.receive(dataPacket);
-				data = dataPacket.getData();
+				dataReciever.receive(dataRecieved);
+				currentData = dataRecieved.getData();
 
-				//	Set and increment size...
+				//	Set ALL the things...
 				int startSize = size;
-				size += dataPacket.getData().length -2;
+				size += dataRecieved.getData().length -2;
 				size++;
 
-				//	Ouptut info...
-				System.out.println("Sequence number: " + data[0] +", Offset start: " + startSize + ", Offset end: " + size);
-				System.arraycopy(data, 1, finalData, 0+(loopCounter*9999), 9999);
+				//	Ouptut info for user...
+				System.out.println("Sequence number: " + currentData[0] +", Offset start: " + startSize + ", Offset end: " + size);
+				System.arraycopy(currentData, 1, finalData, 0+(loopCounter*9999), 9999);
+
+				//	Send Acknowledgement Packet back to Data Sender
+				dataReciever.send(new DatagramPacket(new byte[] {0}, 1, new InetSocketAddress("localhost", 8080)));
 				
 			}
 
 			//	Increment iterator no matter what
 			loopCounter++;   
-			
+
 		}
 
 		//	Write the data...
@@ -147,7 +152,7 @@ class PacketReceiver {
 		//	Close streams so no data leaks...
 		dataReciever.close();
 		stream.close();
-		
+
 	}
 
 }
--- src/PacketSender.java
+++ src/PacketSender.java
@@ -8,70 +8,143 @@
 import java.io.File;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.nio.file.Files;
 
 class PacketSender {
+	
+	//	HOW TO RUN PROGRAM EXAMPLE:
+	//	java PacketSender -s 100 -t 30000 -d 0.25 20.20.20.20 8024
 
-	  public static void main(String[] args) throws Exception {
-		  
-		//	Initialize socket for sending and file to send...
-  		DatagramSocket dataSender = new DatagramSocket(8080);
-  		
-  		//	Initialize file to send...
-		File file = new File("../alice29.txt");
-	    byte[] fileData = Files.readAllBytes(file.toPath());
+	//	THINGS NEEDED TO BE DONE:
+	//	1. Implement all args
+	//	2. Implement drop/corrupt/discard rate in dataReceiver
+	//	3. Get rid of leftover packet bullshit
+	//	4. Implement timeout
 
-		//	Initialize content buffers...
-	    byte[] data = new byte[10000];
-	    
-	    //	accumulator1 is just the last unsized part of the sender...
-	    byte[] leftoverData = new byte[7284];
-	    
-	    // used to keep track of total datagrams sent 
-	    byte dataGramAccumulator = 1;
-	    
-	    // j is a counter to keep track of location of buffer for each datagram
-	    int j = 1; 
-	    int k=0;
-	    
-	    //	Output for user...
-	    System.out.println("packets sent:");
-	    
-	    // for loop used to write data from the input file into individual datagrams
-	    for (int i = 1; i < fileData.length; i++,j++) {
-	    	
-	    	//	Give accumulator what's currently in dataGramAccumulator @ pos i...
-	    	data[0] = dataGramAccumulator;
-	    	data[j] = fileData[i];
-	    	    	
-	    	if (j%9999 == 0) {
-	    		DatagramPacket packet = new DatagramPacket(data, data.length, new InetSocketAddress("localhost", 8024));
-	    		dataSender.send(packet);
-	    		
-	    		System.out.println( "[SENDing]: Sequence number: " + data[0] + ", " +
-	    							"Offset start: " + (0+9999*k) + ", " +
-	    							"Offset end: " + (9998+9999*k)
-	    		);
-	    		
-	    		k++;
-	    		dataGramAccumulator++;
-	    		j=1;
-	    	}
-	    }
-	    
-	    // temp is the size of what is remaining in the input file after full size datagrams are sent
-	    int temp = fileData.length-(dataGramAccumulator-1)*9999;
-	    
-	    // send the last datagram that is not of full size
-	    System.arraycopy(data, 0, leftoverData, 0, 7284);
-	    DatagramPacket leftoverContainter = new DatagramPacket(leftoverData, temp, new InetSocketAddress("localhost", 8024));
-		dataSender.send(leftoverContainter);
-		
-		// Output for user...
-		System.out.println("Sequence number: " + leftoverData[0] +", Offset start: " + (fileData.length-temp) + ", Offset end: " + (fileData.length-1));
-		
-	    //	Close streams so no data leaks...
-	    dataSender.close();
-	  }
+	//					  LET'S 'A GO~
+	//	
+	//	─────────────────────███████──███████
+	//	─────────────────████▓▓▓▓▓▓████░░░░░██
+	//	───────────────██▓▓▓▓▓▓▓▓▓▓▓▓██░░░░░░██
+	//	─────────────██▓▓▓▓▓▓████████████░░░░██
+	//	───────────██▓▓▓▓▓▓████████████████░██
+	//	───────────██▓▓████░░░░░░░░░░░░██████
+	//	─────────████████░░░░░░██░░██░░██▓▓▓▓██
+	//	─────────██░░████░░░░░░██░░██░░██▓▓▓▓██
+	//	───────██░░░░██████░░░░░░░░░░░░░░██▓▓██
+	//	───────██░░░░░░██░░░░██░░░░░░░░░░██▓▓██
+	//	─────────██░░░░░░░░░███████░░░░██████
+	//	───────────████░░░░░░░███████████▓▓██
+	//	─────────────██████░░░░░░░░░░██▓▓▓▓██
+	//	───────────██▓▓▓▓██████████████▓▓██
+	//	─────────██▓▓▓▓▓▓▓▓████░░░░░░████
+	//	───────████▓▓▓▓▓▓▓▓██░░░░░░░░░░██
+	//	───────████▓▓▓▓▓▓▓▓██░░░░░░░░░░██
+	//	───────██████▓▓▓▓▓▓▓▓██░░░░░░████████
+	//	─────────██████▓▓▓▓▓▓████████████████
+	//	───────────██████████████████████▓▓▓▓██
+	//	─────────██▓▓▓▓████████████████▓▓▓▓▓▓██
+	//	───────████▓▓██████████████████▓▓▓▓▓▓██
+	//	───────██▓▓▓▓██████████████████▓▓▓▓▓▓██
+	//	───────██▓▓▓▓██████████──────██▓▓▓▓████
+	//	───────██▓▓▓▓████──────────────██████ 
+	//	─────────████
+	
+	static int datagramsToCurrupt = 0;
+	static InetAddress receiver_ip_addr;
+	static int receiver_port = 8024;
+	
+	private static void setArgs(String[] args) throws UnknownHostException {
+
+		//	Test if we want to set...
+		if (args.length > 3 && args[0].endsWith("d")) {
+
+			//	Set static variable from args...
+			datagramsToCurrupt = (int) (Double.parseDouble(args[1]) * 100);
+			receiver_ip_addr = InetAddress.getByName(args[2]);
+			receiver_port = Integer.parseInt(args[3]);
+
+		}
+
 	}
+
+	public static void main(String[] args) throws Exception {
+		
+		//	Set args...
+		setArgs(args);
+
+		//	Initialize socket for sending and file to send...
+		DatagramSocket dataSender = new DatagramSocket(8080);
+				
+		// Initialize data sizes and Datagram Packets for storage
+		byte[] data = new byte[10000];
+		DatagramPacket dataPacket = new DatagramPacket(data, data.length);
+
+		//	Initialize file to send...
+		File file = new File("../alice29.txt");
+		byte[] fileData = Files.readAllBytes(file.toPath());
+
+		//	accumulator1 is just the last unsized part of the sender...
+		byte[] leftoverData = new byte[7284];
+
+		// used to keep track of total datagrams sent 
+		byte dataGramAccumulator = 1;
+
+		// j is a counter to keep track of location of buffer for each datagram
+		int j = 1; 
+		int k=0;
+
+		//-------------------------------------------------------
+		//														|
+		// 	AREA BELOW IS WHERE SHIT IS DONE					|
+		//														|
+		//-------------------------------------------------------
+		
+		for (int i = 1; i < fileData.length; i++,j++) {
+
+			//	Give accumulator what's currently in dataGramAccumulator @ pos i...
+			data[0] = dataGramAccumulator;
+			data[j] = fileData[i];
+
+			if (j%9999 == 0) {
+				dataSender.send(new DatagramPacket(data, data.length, new InetSocketAddress("localhost", 8024)));
+
+				System.out.println( "[SENDing]: Sequence number: " + data[0] + ", " +
+									"Offset start: " + (0+9999*k) + ", " +
+									"Offset end: " + (9998+9999*k));
+
+				k++;
+				dataGramAccumulator++;
+				j=1;
+				
+//				Wait for ACK...
+				byte[] ackData = new byte[1];
+				DatagramPacket ackPacket = new DatagramPacket(new byte[1], 1);
+				dataSender.receive(ackPacket);
+				ackData = ackPacket.getData();
+				if (ackData[0] == 0) {
+					System.out.println("ACK");
+				}
+			}
+		}
+
+		// temp is the size of what is remaining in the input file after full size datagrams are sent
+		int temp = fileData.length-(dataGramAccumulator-1)*9999;
+
+		// send the last datagram that is not of full size
+		System.arraycopy(data, 0, leftoverData, 0, 7284);
+		dataSender.send(new DatagramPacket(leftoverData, temp, new InetSocketAddress("localhost", 8024)));
+		
+
+		// Output for user...
+		System.out.println("[SENDing]: Sequence number: " + leftoverData[0] + ", " +
+							"Offset start: " + (fileData.length-temp) + ", " +
+							"Offset end: " + (fileData.length-1));
+		
+		//	Close streams so no data leaks...
+		dataSender.close();
+	}
+}
