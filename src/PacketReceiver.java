9bd6ab5b7a0b77e05f7870a214d592a3232ab112
Nathan <nate.gjerd@icloud.com>
2019-03-30 13:59:03 -0500

    Part 2 setup
    Added comments to make code more understandable. Added more parameters for Reciever.

--- src/PacketReceiver.java
+++ src/PacketReceiver.java
@@ -4,52 +4,100 @@
  */
 
 import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 
 class PacketReceiver {
-  public static void main(String[] args) throws Exception {
-    byte[] buffer = new byte[10000];            // to store incoming individual datagram data
-    byte[] buffer1 = new byte[7284];
-    byte[] bufferFinal = new byte[107273];	// used to concatenate all datagram data
-    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-    DatagramPacket packet1 = new DatagramPacket(buffer1, buffer1.length);
-    DatagramSocket socket = new DatagramSocket(8024);
-    int i=0;
-    int size=0;
-    System.out.println("packets received:");
-    // loop to receive all datagrams being sent by the sending program
-    while (i<11) {
-       
-        // final packet is a different size so this is used to store the appropriate size of the packet
-        if(i==10) {
-        	socket.receive(packet1);
-        	buffer1 = packet1.getData();
-        	int startSize = size;
-            size += packet1.getData().length -2;
-            System.out.println("Sequence number: " + buffer1[0] +", Offset start: " + startSize + ", Offset end: " + size);
-            System.arraycopy(buffer1, 1, bufferFinal, 0+(i*9999), 7283);
-         // used to store all packets of full size being received from sender
-        }else {
-        	socket.receive(packet);
-        	buffer = packet.getData();
-        	int startSize = size;
-        	size += packet.getData().length -2;
-        	System.out.println("Sequence number: " + buffer[0] +", Offset start: " + startSize + ", Offset end: " + size);
-        	size++;
-        	System.arraycopy(buffer, 1, bufferFinal, 0+(i*9999), 9999);
-        }
-        i++;   
-   }
-    //write the data to the save file
-    FileOutputStream stream = new FileOutputStream("output.PNG");
-    stream.write(bufferFinal);
-    stream.close();
-    
-    
-    
-  }
+
+	//	Initialize static variables with default vals...
+	static double datagramsToCurrupt = 0;
+	static InetAddress receiver_ip_addr;
+	static int receiver_port = 8024;
+
+	private static void getArgs(String[] args) throws UnknownHostException {
+		datagramsToCurrupt = Double.parseDouble(args[0]);
+		receiver_ip_addr = InetAddress.getByName(args[1]);
+		receiver_port = Integer.parseInt(args[2]);
+	}
+
+
+	public static void main(String[] args) throws Exception {
+
+		getArgs(args);
+
+		// Initialize Buffer sizes
+		//	WHERE DO WE GET THESE SIZES?!?! Make then calculate-able. 
+		byte[] buffer = new byte[10000];
+		byte[] buffer1 = new byte[7284];
+
+		// Final buffer size to contain data (buffer + buffer1) 
+		byte[] bufferFinal = new byte[107273];	
+
+		//	Datagram Packets for storage
+		//	HOW ARE WE INITIALIZING THESE TO MAKE SENSE?
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+		DatagramPacket packet1 = new DatagramPacket(buffer1, buffer1.length);
+
+		//	Something...? Maybe starting a socket on this port?
+		//	Eventually want this syntax below...
+//		DatagramSocket socket = new DatagramSocket(receiver_port, receiver_ip_addr);
+		DatagramSocket socket = new DatagramSocket(receiver_port);
+
+		//	Initial looping value and size...
+		int i=0;
+		int size=0;
+
+		//	Starting output...
+		System.out.println("packets received:");
+
+		// Loop for when datapackets are recieved...
+		while (i<11) {
+
+			// final clause...
+			if (i==10) {
+				//	Recieve info...
+				socket.receive(packet1);
+				buffer1 = packet1.getData();
+
+				//	Set and increment size...
+				int startSize = size;
+				size += packet1.getData().length -2;
+
+				//	Ouptut info...
+				System.out.println("Sequence number: " + buffer1[0] +", Offset start: " + startSize + ", Offset end: " + size);
+				System.arraycopy(buffer1, 1, bufferFinal, 0+(i*9999), 7283);
+			} else {
+
+				//	Recieve info...
+				socket.receive(packet);
+				buffer = packet.getData();
+
+				//	Set and increment size...
+				int startSize = size;
+				size += packet.getData().length -2;
+				size++;
+
+				//	Ouptut info...
+				System.out.println("Sequence number: " + buffer[0] +", Offset start: " + startSize + ", Offset end: " + size);
+				System.arraycopy(buffer, 1, bufferFinal, 0+(i*9999), 9999);
+			}
+
+			//	Increment iterator no matter what
+			i++;   
+		}
+
+		//	Write the data...
+		FileOutputStream stream = new FileOutputStream("output.txt");
+		stream.write(bufferFinal);
+
+		//	Close streams so no data leaks...
+		socket.close();
+		stream.close();
+	}
+
+
+
 }
--- src/PacketSender.java
+++ src/PacketSender.java
@@ -15,23 +15,35 @@ class PacketSender {
 
 	  public static void main(String[] args) throws Exception {
 		  
+		//	Initialize socket for sending and file to send...
   		DatagramSocket socket = new DatagramSocket(8080);
-		File file = new File(args[0]);
+  		
+  		//	Initialize file to send...
+		File file = new File("../alice29.txt");
 	    byte[] fileContent = Files.readAllBytes(file.toPath());
+
+		//	Initialize content buffers...
 	    byte[] accumulator = new byte[10000];
+	    
+	    //	accumulator1 is just the last unsized part of the sender...
 	    byte[] accumulator1 = new byte[7284];
+	    
 	    // used to keep track of total datagrams sent 
 	    byte dataGramAccumulator = 1;
+	    
 	    // j is a counter to keep track of location of buffer for each datagram
 	    int j = 1; 
 	    int k=0;
 	    
+	    //	Output for user...
 	    System.out.println("packets sent:");
+	    
 	    // for loop used to write data from the input file into individual datagrams
-	    for (int i = 1; i < fileContent.length; i++) {
+	    for (int i = 1; i < fileContent.length; i++,j++) {
+	    	
+	    	//	Give accumulator what's currently in dataGramAccumulator @ pos i...
 	    	accumulator[0] = dataGramAccumulator;
 	    	accumulator[j] = fileContent[i];
-	    	j++;
 	    	    	
 	    	if (j%9999 == 0) {
 	    		DatagramPacket packet = new DatagramPacket(accumulator, accumulator.length, new InetSocketAddress("localhost", 8024));
@@ -42,15 +54,19 @@ class PacketSender {
 	    		j=1;
 	    	}
 	    }
+	    
 	    // temp is the size of what is remaining in the input file after full size datagrams are sent
 	    int temp = fileContent.length-(dataGramAccumulator-1)*9999;
+	    
 	    // send the last datagram that is not of full size
 	    System.arraycopy(accumulator, 0, accumulator1, 0, 7284);
 	    DatagramPacket packet = new DatagramPacket(accumulator1, temp, new InetSocketAddress("localhost", 8024));
 		socket.send(packet);
+		
+		// Output for user...
 		System.out.println("Sequence number: " + accumulator1[0] +", Offset start: " + (fileContent.length-temp) + ", Offset end: " + (fileContent.length-1));
 		
-		// close the socket being used
+	    //	Close streams so no data leaks...
 	    socket.close();
 	  }
 	}
