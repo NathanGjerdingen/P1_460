b4d31369ee7144de31534d58a89506cdbb6de371
JoseMorales27 <07jose.m@gmail.com>
2019-04-02 13:37:18 -0500

    Comments

--- src/PacketReceiver.java
+++ src/PacketReceiver.java
@@ -1,7 +1,3 @@
-/** @author Tyler, Nate, Jose
- * Program to recieve datagram packets and display the order they were received
- * then output the data recieved to a save file
- */
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -15,127 +11,172 @@ import java.util.Random;
 
 import java.util.Base64;
 
-
+/**
+ * Program will receive data packets from a Sender. It will deteermine if the
+ * packets are corrupt, dropped, or good and send an ack to the Sender. It will
+ * then write the packets received to an outfile.
+ * 
+ * @author Tyler, Nate, Jose
+ *
+ */
 class PacketReceiver {
 
-	//	HOW TO RUN PROGRAM EXAMPLE:
-	//	java PacketReceiver -d 0.5 127.0.0.1 8024
+	// HOW TO RUN PROGRAM EXAMPLE:
+	// java PacketReceiver -d 0.5 127.0.0.1 8024
 
-	//	Initialize static variables with default vals...
+	// Initialize static variables with default vals...
 	static int datagramsToCurrupt = 0;
 	static InetAddress receiver_ip_addr;
 	static int receiver_port = 8024;
 
-	//	Flags for ACK's
+	// Flags for ACK's
 	static final int GOOD = 0;
 	static final int CORRUPT = 1;
 	static final int MOVEWND = 2;
 	static final int DROP = 3;
 	static final int COMPLETE = 4;
 
-
+	/**
+	 * This method takes 3 arguments. 1st will be the percentage of datagrams to
+	 * corrupt. 2nd is the IP address. 3rd is the Port. It will assign them to the
+	 * correct variables.
+	 * 
+	 * @param args
+	 * @throws UnknownHostException
+	 */
 	private static void setArgs(String[] args) throws UnknownHostException {
 
-		//	Test if we want to set...
+		// Test if we want to set...
 		if (args.length > 3 && args[0].endsWith("d")) {
 
-			//	Set static variable from args...
+			// Set static variable from args...
 			datagramsToCurrupt = (int) (Double.parseDouble(args[1]) * 100);
 			receiver_ip_addr = InetAddress.getByName(args[2]);
 			receiver_port = Integer.parseInt(args[3]);
 		}
 	}
 
+	/**
+	 * Main method where all the work is done. Starts by call the setArgs method.
+	 * Then will start the Receiver for the packets that will be recieved. It will
+	 * then receive a first packet with the information of the size of datagrams to
+	 * expect. It will use the size to create a byte array to store the packets
+	 * being received. It will then loop with every packet and determine if its
+	 * corrupt, dropped or good and send the appropriate ack to the Sender. Loop
+	 * will end when Sender indicates that it is done.
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
 
-		//	Set args...
+		// Set args...
 		setArgs(args);
 
-		//	Init dataReciever...
+		// Init dataReciever...
 		DatagramSocket dataReciever = new DatagramSocket(receiver_port, receiver_ip_addr);
 
-		//	Initial looping value and size...
+		// Initial looping value and size...
 		int size = 0;
 
 		// Initialize Alice...
 		File file = new File("alice29.txt");
 
 		// Initialize Output...
-		//		PrintWriter stream = new PrintWriter("../output.txt");
+		// PrintWriter stream = new PrintWriter("../output.txt");
 		FileOutputStream stream = new FileOutputStream("output.txt");
 
-		//	Starting output...
+		// Starting output...
 		System.out.println("Awaiting data...");
 
-		//	Recieve sizing flags...
+		// Recieve sizing flags...
 		byte[] something = new byte[1];
 		DatagramPacket info = new DatagramPacket(something, something.length);
 		something = info.getData();
 		dataReciever.receive(info);
-		int dataSize = something[0];		
+		int dataSize = something[0];
 		int loopAmount = ((int) file.length() / dataSize);
 
 		// Initialize data sizes and Datagram Packets for storage
 		byte[] currentData = new byte[dataSize];
 		DatagramPacket dataRecieved = new DatagramPacket(currentData, currentData.length);
 
-		// Final data size to contain data (data + leftoverData) 
+		// Final data size to contain data (data + leftoverData)
 		byte[] finalData = new byte[(int) file.length()];
-		byte[] writeData = new byte[dataSize-2];
+		byte[] writeData = new byte[dataSize - 2];
 
-		//-------------------------------------------------------
-		//														|
-		// 	AREA BELOW IS WHERE SHIT IS DONE					|
-		//														|
-		//-------------------------------------------------------
-		
+		// -------------------------------------------------------
+		// |
+		// AREA BELOW IS WHERE SHIT IS DONE |
+		// |
+		// -------------------------------------------------------
+
+		// Allowing the loop to run until packet to break is received.
 		boolean openToRecieve = true;
 		while (openToRecieve) {
 
-			//	When recieving data...
+			// When recieving data...
 			dataReciever.receive(dataRecieved);
 			currentData = dataRecieved.getData();
-			
+
+			// Used to break from the while when packet contains flag to complete.
 			if (currentData[1] == COMPLETE) {
 				break;
 			}
 
+			// If the packet contains a corrupt flag then send appropriate ack to the
+			// Sender so that it will try again. Display a message indicating that we got
+			// a corrupt packet.
 			if (currentData[1] == CORRUPT) {
-				dataReciever.send(new DatagramPacket(new byte[] {CORRUPT}, 1, new InetSocketAddress("localhost", 8080)));
+				dataReciever
+						.send(new DatagramPacket(new byte[] { CORRUPT }, 1, new InetSocketAddress("localhost", 8080)));
 				System.out.println("[CRPT]: Sequence number: " + currentData[0] + " requesting resend");
 				dataReciever.receive(dataRecieved);
 				currentData = dataRecieved.getData();
 			}
 
+			// If the packet has a dropped flag set then we send the appropriate ack to the
+			// Sender so it will try again. Display a message indicating that a packet was
+			// dropped.
 			if (currentData[1] == DROP) {
-				dataReciever.send(new DatagramPacket(new byte[] {DROP}, 1, new InetSocketAddress("localhost", 8080)));
+				dataReciever.send(new DatagramPacket(new byte[] { DROP }, 1, new InetSocketAddress("localhost", 8080)));
 				System.out.println("[TO]: Sequence number: " + currentData[0] + " requesting resend");
 				dataReciever.receive(dataRecieved);
 				currentData = dataRecieved.getData();
 			}
 
-			boolean run= true;
-			while(run) {
+			// Randomizing corrupt packets. While will only run if corrupt packets are being
+			// sent. Once a good packet is received then it will break from the while.
+			boolean run = true;
+			while (run) {
 				int rand = new Random().nextInt(101);
 				if (rand <= datagramsToCurrupt) {
-					if (rand%2 == 0) {
-						dataReciever.send(new DatagramPacket(new byte[] {CORRUPT}, 1, new InetSocketAddress("localhost", 8080)));
+					if (rand % 2 == 0) {
+						dataReciever.send(new DatagramPacket(new byte[] { CORRUPT }, 1,
+								new InetSocketAddress("localhost", 8080)));
 						dataReciever.receive(dataRecieved);
 						currentData = dataRecieved.getData();
 					}
 				} else {
-					dataReciever.send(new DatagramPacket(new byte[] {GOOD}, 1, new InetSocketAddress("localhost", 8080)));
+					dataReciever
+							.send(new DatagramPacket(new byte[] { GOOD }, 1, new InetSocketAddress("localhost", 8080)));
 					run = false;
 				}
 			}
+			// Displaying the packet information. It includes sequence number, offset start,
+			// offset send.
 			int startSize = size;
-			size += dataRecieved.getData().length -3;
+			size += dataRecieved.getData().length - 3;
 			size++;
-			System.out.println("[RECV]: Sequence number: " + currentData[0] +", Offset start: " + startSize + ", Offset end: " + size);
-			System.arraycopy(currentData, 2, writeData, 0, dataSize-2 );
+			System.out.println("[RECV]: Sequence number: " + currentData[0] + ", Offset start: " + startSize
+					+ ", Offset end: " + size);
+			// Getting a copy of the data that was received and writing it to the output
+			// file.
+			System.arraycopy(currentData, 2, writeData, 0, dataSize - 2);
 			stream.write(writeData);
 		}
 
+		// Closing the receiver and stream.
 		dataReciever.close();
 		stream.close();
 	}
--- src/PacketSender.java
+++ src/PacketSender.java
@@ -1,9 +1,3 @@
-/** @author Tyler, Nate, Jose
- * Program to send datagram packets and display the order they were sent to the
- * receiving program
- * Program Requirements: the program needs the input file testsheet.PNG to be passed
- * as an argument.
- */
 
 import java.io.File;
 import java.net.DatagramPacket;
@@ -14,14 +8,24 @@ import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.util.Random;
 
+/**
+ * Program will read a file and put the data in a byte array. Data will then be
+ * sent to the receiver according the specified datagram size. It will then wait
+ * for a response for a successful ack, corrupt message or dropped message from
+ * the Receiver before sending any more packets. Once complete we will notify
+ * the Receiver that no packets will be sent.
+ * 
+ * @author Tyler, Nate, Jose
+ *
+ */
 class PacketSender {
 
-	//	HOW TO RUN PROGRAM EXAMPLE:
-	//	java PacketSender -s 100 -t 30000 -d 0.25 127.0.0.1 8024
+	// HOW TO RUN PROGRAM EXAMPLE:
+	// java PacketSender -s 100 -t 30000 -d 0.25 127.0.0.1 8024
 
-	//	Initialize static variables with default vals...
+	// Initialize static variables with default vals...
 	static int datagramSize = 10000;
-	static int datagramTimeout = 0; 
+	static int datagramTimeout = 0;
 	static int datagramsToCurrupt = 0;
 	static InetAddress receiver_ip_addr;
 	static int receiver_port = 8024;
@@ -31,66 +35,89 @@ class PacketSender {
 	static final int MOVEWND = 2;
 	static final int DROP = 3;
 	static final int COMPLETE = 4;
-	
+
+	/**
+	 * This method will take 5 arguments from command line. 1st arg is datagram
+	 * size, 2nd will be timeout interval, 3rd will be percentage of datagrams to
+	 * corrupt, 4th is the IP address and 5th is the Port. It will assign them to
+	 * the correct variables.
+	 * 
+	 * @param args
+	 * @throws UnknownHostException
+	 */
 	private static void setArgs(String[] args) throws UnknownHostException {
 
-		//	Test if we want to set...
-		if (args.length > 6  && args[0].endsWith("s") && 
-				args[2].endsWith("t") && args[4].endsWith("d")) {
+		// Test if we want to set...
+		if (args.length > 6 && args[0].endsWith("s") && args[2].endsWith("t") && args[4].endsWith("d")) {
 
-			//	Set static variable from args...
+			// Set static variable from args...
 			datagramSize = Integer.parseInt(args[1]);
-			datagramTimeout = Integer.parseInt(args[3]); 
+			datagramTimeout = Integer.parseInt(args[3]);
 			datagramsToCurrupt = (int) (Double.parseDouble(args[1]) * 100);
 			receiver_ip_addr = InetAddress.getByName(args[6]);
 			receiver_port = Integer.parseInt(args[7]);
 		}
 	}
 
+	/**
+	 * Main method is where all the work is done. Begins with calling the setArgs
+	 * method. Sets timeout interval. Creates byte array with datagram size given.
+	 * Opens file that will be sent and reads all the bytes. It will send the
+	 * receiver the size of the datagrams it should expect.
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
 
-		//	Set args...
+		// Set args...
 		setArgs(args);
 
-		//	Initialize socket for sending and file to send...
+		// Initialize socket for sending and file to send...
 		DatagramSocket dataSender = new DatagramSocket(8080);
 		dataSender.setSoTimeout(datagramTimeout);
 
 		// Initialize data sizes and Datagram Packets for storage
 		byte[] data = new byte[datagramSize];
 
-		//	Initialize file to send...
+		// Initialize file to send...
 		File file = new File("alice29.txt");
 		byte[] fileData = Files.readAllBytes(file.toPath());
 		byte dataGramAccumulator = 1;
 
 		// j is a counter to keep track of location of buffer for each datagram
-		int j = 2; 
-		int k=0;
+		int j = 2;
+		int k = 0;
 
-		//-------------------------------------------------------
-		//														|
-		// 	AREA BELOW IS WHERE SHIT IS DONE					|
-		//														|
-		//-------------------------------------------------------
+		// -------------------------------------------------------
+		// |
+		// AREA BELOW IS WHERE SHIT IS DONE |
+		// |
+		// -------------------------------------------------------
 
-		// Send Receiver info. 
-		dataSender.send(new DatagramPacket(new byte[] {(byte) datagramSize}, 1, new InetSocketAddress("localhost", 8024)));
+		// Send Receiver info.
+		dataSender.send(
+				new DatagramPacket(new byte[] { (byte) datagramSize }, 1, new InetSocketAddress("localhost", 8024)));
 
 		System.out.println(datagramsToCurrupt);
-		for (int i = 1; i < fileData.length; i++,j++) {
+		for (int i = 1; i < fileData.length; i++, j++) {
 
-			//	Give accumulator what's currently in dataGramAccumulator @ pos i...
+			// Give accumulator what's currently in dataGramAccumulator @ pos i...
 			data[0] = dataGramAccumulator;
-			data[j] = fileData[i];	
+			data[j] = fileData[i];
+
+			// Creating a packet to send to the Receiver.
 			DatagramPacket packet = new DatagramPacket(data, data.length, new InetSocketAddress("localhost", 8024));
 
-			if (j%(datagramSize-1) == 0) {
+			// Checks if location of buffer has reached the size limit.
+			if (j % (datagramSize - 1) == 0) {
 
+				// Randomizing if the packet will corrupt or if it will be dropped or if its a
+				// good packet.
 				int rand = new Random().nextInt(101);
 
 				if (rand <= datagramsToCurrupt) {
-					if (rand%2 == 0) {
+					if (rand % 2 == 0) {
 						data[1] = CORRUPT;
 						packet.setData(data);
 						dataSender.send(packet);
@@ -105,68 +132,86 @@ class PacketSender {
 					dataSender.send(packet);
 				}
 
-				System.out.println( "[SENDing]: Sequence number: " + data[0] + ", " +
-						"Offset start: " + (0+(datagramSize-2)*k) + ", " +
-						"Offset end: " + ((datagramSize-3)+(datagramSize-2)*k));
+				// Displaying the information of the packets sent.
+				System.out.println("[SENDing]: Sequence number: " + data[0] + ", " + "Offset start: "
+						+ (0 + (datagramSize - 2) * k) + ", " + "Offset end: "
+						+ ((datagramSize - 3) + (datagramSize - 2) * k));
 
 				// Increment ALL the things
 				k++;
 				dataGramAccumulator++;
-				j=2;
+				j = 2;
 
-				//-------------------------------------------------------
-				//														|
-				//  ACK POINT											|
-				//														|
-				//-------------------------------------------------------
+				// -------------------------------------------------------
+				// |
+				// ACK POINT |
+				// |
+				// -------------------------------------------------------
 
+				// Creating an ack packet of 1 byte
 				byte[] ackData = new byte[1];
 				DatagramPacket ackPacket = new DatagramPacket(new byte[1], 1);
+
+				// Receiving an ack packet and reading whether its a good, corrupt or dropped.
 				dataSender.receive(ackPacket);
 				ackData = ackPacket.getData();
-				
+
+				// If its a good packet, displays that successful ack was received.
 				if (ackData[0] == 0) {
-					System.out.println("[AckRcvd]: " + (dataGramAccumulator-1) + " MoveWnd");
+					System.out.println("[AckRcvd]: " + (dataGramAccumulator - 1) + " MoveWnd");
 				}
-				
-				while(ackData[0] !=0) {
+
+				// While we're not getting a successful ack, we will sit in this loop then we
+				// check if its either getting corrupt or dropped.
+				// We will display that we got an error packet and attempt sending the packet.
+				// Once we finally get a successful packet we will display the success and move
+				// on.
+				while (ackData[0] != 0) {
+					// Handle if we get a corrupt packet error.
 					if (ackData[0] == CORRUPT) {
-						System.out.println("[ErrAck]: " + (dataGramAccumulator-1));
+						System.out.println("[ErrAck]: " + (dataGramAccumulator - 1));
 						data[1] = 0;
 						packet.setData(data);
 						dataSender.send(packet);
-						System.out.println( "[RESENDing]: Sequence number: " + data[0] + ", " +
-								"Offset start: " + (0+(datagramSize-2)*k) + ", " +
-								"Offset end: " + ((datagramSize-3)+(datagramSize-2)*k));
+						System.out.println("[RESENDing]: Sequence number: " + data[0] + ", " + "Offset start: "
+								+ (0 + (datagramSize - 2) * k) + ", " + "Offset end: "
+								+ ((datagramSize - 3) + (datagramSize - 2) * k));
 						dataSender.receive(ackPacket);
 						ackData = ackPacket.getData();
+						// If we get a successful ack display messge
 						if (ackData[0] == 0) {
-							System.out.println("[AckRcvd]: " + (dataGramAccumulator-1) + " MoveWnd");
-						} 
+							System.out.println("[AckRcvd]: " + (dataGramAccumulator - 1) + " MoveWnd");
+						}
+						// Handle if we get dropped packet error
 					} else if (ackData[0] == DROP) {
-						System.out.println("[ErrAck]: " + (dataGramAccumulator-1));
+						System.out.println("[ErrAck]: " + (dataGramAccumulator - 1));
 						data[1] = 0;
 						packet.setData(data);
 						dataSender.send(packet);
-						System.out.println( "[RESENDing]: Sequence number: " + data[0] + ", " +
-								"Offset start: " + (0+(datagramSize-2)*k) + ", " +
-								"Offset end: " + ((datagramSize-3)+(datagramSize-2)*k));
+						System.out.println("[RESENDing]: Sequence number: " + data[0] + ", " + "Offset start: "
+								+ (0 + (datagramSize - 2) * k) + ", " + "Offset end: "
+								+ ((datagramSize - 3) + (datagramSize - 2) * k));
 						dataSender.receive(ackPacket);
 						ackData = ackPacket.getData();
+						// If we get a successful ack display messge
 						if (ackData[0] == 0) {
-							System.out.println("[AckRcvd]: " + (dataGramAccumulator-1) + " MoveWnd");
+							System.out.println("[AckRcvd]: " + (dataGramAccumulator - 1) + " MoveWnd");
 						}
 					}
 				}
 			}
 
-			if (i == file.length()-1) {
+			// If we're at the end of the file we will send the last of the data.
+			if (i == file.length() - 1) {
 				dataSender.send(packet);
 			}
 		}
-		data[1]=COMPLETE;
+		// Indicating that we are done sending packets.
+		data[1] = COMPLETE;
 		DatagramPacket closePacket = new DatagramPacket(data, data.length, new InetSocketAddress("localhost", 8024));
 		dataSender.send(closePacket);
+
+		// Closing the sender.
 		dataSender.close();
 
 	}
